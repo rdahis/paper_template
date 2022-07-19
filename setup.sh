1014424fa546153192f87fa1db43b5b52c3e5c8b
Ricardo Dahis <rdahis@gmail.com>
2022-07-19 11:27:10 -0300

    fix: quotes in cd command in setup.sh

--- setup.sh
+++ setup.sh
@@ -3,10 +3,9 @@
 NON_VERSIONED_PATH=""
 REPO_PATH=""
 
-cd $REPO_PATH
+cd "${REPO_PATH}"
 
 ln -s "${NON_VERSIONED_PATH}/input" input
 ln -s "${NON_VERSIONED_PATH}/output/data" output/data
 ln -s "${NON_VERSIONED_PATH}/output/logs" output/logs
 ln -s "${NON_VERSIONED_PATH}/tmp" tmp
-

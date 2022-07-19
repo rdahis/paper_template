280010d509343f7e2fa7f23b593d6acdbcf8cbd5
Ricardo Dahis <rdahis@gmail.com>
2022-07-19 11:49:08 -0300

    fix: adds -h option to ln

--- setup.sh
+++ setup.sh
@@ -3,9 +3,7 @@
 NON_VERSIONED_PATH=""
 REPO_PATH=""
 
-cd "${REPO_PATH}"
-
-ln -s "${NON_VERSIONED_PATH}/input" input
-ln -s "${NON_VERSIONED_PATH}/output/data" output/data
-ln -s "${NON_VERSIONED_PATH}/output/logs" output/logs
-ln -s "${NON_VERSIONED_PATH}/tmp" tmp
+ln -shF "${NON_VERSIONED_PATH}/input"       "${REPO_PATH}/input"
+ln -shF "${NON_VERSIONED_PATH}/output/data" "${REPO_PATH}/output/data"
+ln -shF "${NON_VERSIONED_PATH}/output/logs" "${REPO_PATH}/output/logs"
+ln -shF "${NON_VERSIONED_PATH}/tmp"         "${REPO_PATH}/tmp"
